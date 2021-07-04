5a9ae55c62233bc60c41cbd686b17886fb3d7f11
Piato4ka <74746872+Piato4ka@users.noreply.github.com>
2021-07-05 00:56:51 +0300

    Update Controller.java

--- Java_Number_Game/Controller.java
+++ Java_Number_Game/Controller.java
@@ -12,31 +12,6 @@ public class Controller {
 		this.view = view;
 	}
 
-	void processUser() {
-		if (rightInput()) {
-			model.changeDiapason();
-			System.out.println("You inputed: " + model.playerNumber);
-			helpToFind ();
-		} else {
-			System.out.println(view.WRONG_INPUT_INT_DATA);
-		}
-	}
-
-	void helpToFind () {
-		if (model.playerNumber > model.secretNumber) {
-			view.printMessage(view.LESS_NUMBER, model.min, model.max);
-			
-		}
-		else if (model.playerNumber < model.secretNumber) {
-			view.printMessage(view.GREATER_NUMBER, model.min, model.max);
-		}
-		else if (model.playerNumber == model.secretNumber) {
-			view.printMessage(view.VIN);
-			System.out.println("You made it in " + model.countTries + " steps.");
-		}
-	}
-
-		
 	boolean rightInput() {
 		Scanner sc = new Scanner(System.in);
 		if (sc.hasNextInt()) {
@@ -50,7 +25,34 @@ public class Controller {
 				return true;
 			}
 		} else {
+			System.out.println(view.WRONG_INPUT_INT_DATA);
+			rightInput();
 			return false;
+		}
+	}
+
+	void helpToFind() {
+		if (model.playerNumber > model.secretNumber) {
+			view.printMessage(view.LESS_NUMBER, model.min, model.max);
+
+		} else if (model.playerNumber < model.secretNumber) {
+			view.printMessage(view.GREATER_NUMBER, model.min, model.max);
+		} else if (model.playerNumber == model.secretNumber) {
+			view.printMessage(view.VIN);
+			System.out.println("You made it in " + model.countTries + " steps.");
+		}
+	}
+
+	void processUser() {
+		view.printMessage(view.INPUT_YOUR_NUMBER, model.min, model.max);
+		if (!model.endGame) {
+			while (rightInput()) {
+				model.changeDiapason();
+				view.printMessage(view.INPUTED, model.playerNumber);
+				helpToFind();
+			}
+		} else {
+			view.printMessage(view.VIN);
 		}
 	}
 }
