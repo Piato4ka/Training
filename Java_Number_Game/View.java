5261a5b7a78ad47476c2d002b2c68798753234a4
Piato4ka <74746872+Piato4ka@users.noreply.github.com>
2021-07-05 00:58:38 +0300

    Update View.java

--- Java_Number_Game/View.java
+++ Java_Number_Game/View.java
@@ -2,18 +2,23 @@ package Java_Number_Game;
 
 public class View {
 
-	public final String INPUT_YOUR_NUMBER = "Try to gess number. Input your number ";
-    public final String WRONG_INPUT_INT_DATA = "Wrong input! Repeat please: ";
-    public final String GREATER_NUMBER = "Greater nuber, try again from ";
-    public final String LESS_NUMBER = "Less nuber, try again from ";
-    public final String VIN = "It is a VICTORY!";
-    
-    
-    public void printMessage(String message){
-        System.out.println(message);
-    }
+	public final String INPUT_YOUR_NUMBER = "Try to gess number. Input your number from ";
+	public final String WRONG_INPUT_INT_DATA = "Wrong input! Repeat please: ";
+	public final String GREATER_NUMBER = "Greater nuber, try again from ";
+	public final String LESS_NUMBER = "Less nuber, try again from ";
+	public final String VIN = "It is a VICTORY!";
+	public final String INPUTED = "You inputed: ";
 
-    public void printMessage(String message, int min, int max){
-        System.out.println(message + min + " to " + max);
-    }
+	public void printMessage(String message) {
+		System.out.println(message);
+	}
+
+	public void printMessage(String message, int playerNumber) {
+		System.out.println(message + playerNumber);
+	}
+
+	public void printMessage(String message, int min, int max) {
+		System.out.println(message + min + " to " + max);
+	}
 }
+
